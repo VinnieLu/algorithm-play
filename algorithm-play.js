df3b3eff63e016af26099fcef57ce5bc52bfeff0
Vincent Lu <vincentlu@Vincents-MBP.attlocal.net>
2018-02-05 18:33:32 -0600

    completed fibonacci(easy) and fibonacci modified

--- algorithm-play.js
+++ algorithm-play.js
@@ -264,7 +264,7 @@ Your function productFib takes an integer (prod) and returns an array:
 [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
 depending on the language if F(n) * F(n+1) = prod.
 
-If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prod you will return
+If you dont find two consecutive F(m) verifying F(m) * F(m+1) = prod you will return
 
 [F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
 F(m) being the smallest one such as F(m) * F(m+1) > prod.
